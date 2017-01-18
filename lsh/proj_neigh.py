e293e7cf42758d551286f902ee8dbe4b92abd226
Debajyoti Sinha <debsin@users.noreply.github.com>
2017-01-18 18:41:35 +0530

    Add files via upload

---
+++ lsh/proj_neigh.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy import io
+from sklearn.neighbors import NearestNeighbors, LSHForest
+from igraph import Graph, EdgeSeq
+import random
+
+
+random.seed(100)
+
+print "Reading sparce matrix..."
+matrix = io.mmread("whole_matrix")
+print "Converting matrix to dense format..."
+a = np.array(matrix.todense())
+print a.shape
+
+with open("subsamples_louv_68K") as f:
+  idx = f.read().splitlines()
+  
+with open("pc_gene_ids_sub.csv") as f:
+  idx_gene = f.read().splitlines()
+
+idx = np.array(map(int, idx[1:])) - 1
+idx_gene = np.array(map(int, idx_gene[1:])) - 1
+
+print "Initialize LSH..."
+lshf = LSHForest(random_state=42, n_neighbors=5)
+print "fit LSH..."
+lshf.fit(a[idx[:,None],idx_gene])
+
+distances, indices = lshf.kneighbors(a[:,idx_gene], n_neighbors=5)
+
+np.savetxt("neigh.txt",indices)
+
+
+#lshf = LSHForest(random_state=42, n_neighbors=5)
+#lshf.fit(a[:,idx_gene])
+# K= lshf.kneighbors_graph(a[idx[:,None],idx_gene])
+# 
+# print "convert into adjacency matrix..."
+# 
+# K = K.toarray()
+# 
+# g = Graph.Adjacency(K.tolist())
+# es = EdgeSeq(g)
+# 
+# print "writing graph edgelist..."
+# g.write_edgelist("src_dst_lsh_ss.csv")
