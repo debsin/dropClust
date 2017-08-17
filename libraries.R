624fb47e0aba5556baee785edd1ae3fb0c97a308
Debajyoti Sinha <deb.sinha@hotmail.com>
2017-08-17 18:29:16 +0530

    Update libraries.R

--- libraries.R
+++ libraries.R
@@ -1,5 +1,5 @@
 list.of.packages <- c("Matrix", "ggplot2", "Rtsne", "svd", "dplyr", "plyr",
-                      "data.table", "mclust", "flexclust", "reshape2",
+                      "data.table", "mclust", "flexclust", "reshape2","fastcluster",
                       "irlba","dynamicTreeCut", "RColorBrewer")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -11,8 +11,9 @@ library(Matrix)
 library(ggplot2)
 library(Rtsne)
 library(svd)
-library(dplyr)
 library(plyr)
+library(dplyr)
+library(fastcluster)
 library(data.table)
 library(mclust)
 library(flexclust)
